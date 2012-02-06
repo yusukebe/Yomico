eca2c1b8f1e274945de3517a28bf2aa61d49990d
Yusuke Wada <yusuke@kamawada.com>
2012-02-06 18:04:31 +0900

    initial commit

---
+++ lib/Yomico.pm
@@ -0,0 +1,32 @@
+package Yomico;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+Yomico -
+
+=head1 SYNOPSIS
+
+  use Yomico;
+
+=head1 DESCRIPTION
+
+Yomico is
+
+=head1 AUTHOR
+
+Yusuke Wada E<lt>yusuke@kamawada.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
